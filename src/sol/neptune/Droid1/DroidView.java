80b6b6f51beea0fa73d8ac752328531f53feaa36
Christian Peter <peter.chr@web.de>
2013-07-07 13:56:38 +0200

    compass demo

--- src/sol/neptune/Droid1/DroidView.java
+++ src/sol/neptune/Droid1/DroidView.java
@@ -6,11 +6,44 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
-public class DroidView extends View{
+public class DroidView extends View {
+	private float bearing;
 
-	
+	public void setBearing(float _bearing) {
+		bearing = _bearing;
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+	}
+
+	public float getBearing() {
+		return bearing;
+	}
+
+	private float pitch;
+
+	public void setPitch(float _pitch) {
+		pitch = _pitch;
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	private float roll;
+
+	public void setRoll(float _roll) {
+		roll = _roll;
+		sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+
 	private Paint paint = new Paint();
+
 	public DroidView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		paint.setColor(Color.BLACK);
@@ -20,10 +53,12 @@ public class DroidView extends View{
 	public DroidView(Context context) {
 		super(context);
 	}
-	
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
-		canvas.drawText("TEST", 0, 50, paint);
+		canvas.drawText("B:" + getBearing(), 0, 50, paint);
+		canvas.drawText("P:" + getPitch(), 0, 75, paint);
+		canvas.drawText("R:" + getRoll(), 0, 100, paint);
+		
 	}
 }
